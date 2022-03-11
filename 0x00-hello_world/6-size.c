dc9dec74f8a36e671a9b43333cf01e93aecad63d
quadritech <qmujeeb193@gmail.com>
2022-03-10 20:27:02 -0800

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.c>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+char c;
+int i;
+long int a;
+long long int b;
+float f;
+
+printf("the size of a char is: %lu. byte(s)(\n, (unsigned long)sizeof(c));
+printf("the size of a int is: %lu. byte(s)\n, (unsigned long)sizeof(i));
+printf("the size of a long int is: %lu. byte(s)\n, (unsigned long)sizeof(a));
+printf("the size of a long long int is: %lu. byte(s)\n, (unsigned long)sizeof(b));
+printf("the size of a float is: %lu. byte(s)\n, (unsigned long)sizeof(f));
+
+return (0);
+}
