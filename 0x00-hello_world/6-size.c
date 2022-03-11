2257f671e90908f893826a979a465b1829323d75
quadritech <qmujeeb193@gmail.com>
2022-03-10 20:42:26 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int a;
 long long int b;
 float f;
 
-printf("Size of an char is: %lu byte(s)(\n", (unsigned long)sizeof(c));
+printf("Size of a char is: %lu byte(s)(\n", (unsigned long)sizeof(c));
 printf("Size of an int is: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of an long int is: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an long long int is: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of an float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 }
