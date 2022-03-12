5eb6c38527360e2a0e9ad387c6d998a17b0dd53f
quadritech <qmujeeb193@gmail.com>
2022-03-12 14:13:14 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * to print all single digit of base 10
+ * Return: 0
+ */
+int main(void)
+{
+int num = 48;
+
+while (num < 57)
+{
+	putchar("num");
+	num++
+}
+putchar("\n");
+return (0):
+}
