316cec730fd74d45cceae87f596570277022837a
quadritech <qmujeeb193@gmail.com>
2022-03-12 10:37:03 -0800

    Alphabet Game

---
+++ 0x01-variables_if_else_while/2-prints_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: print alphabets in lower case
+ *Return: 0
+ */
+int main(void)
+
+char letters = 'a'
+
+while  
+{
+	letters = "a <= z"
+	{
+		putchar("a-z\n");
+	}
+	return (0);
+}
