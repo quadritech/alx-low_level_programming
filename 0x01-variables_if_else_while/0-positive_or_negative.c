c3f59bfb13c8ea358b0fab58d023e78e45715d05
quadritech <qmujeeb193@gmail.com>
2022-03-11 18:24:52 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,7 @@
 /**
  *main - entry point
  *
- *  to print whether the number stored in the variable n is positive or negative.
- *
+ *  to print whether the number stored in the variable n is positive or negative
  *Return: 0
  */
 int main(void)
