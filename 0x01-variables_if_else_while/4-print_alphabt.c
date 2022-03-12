0bccd24050d9ca4a8004c64434ecd6c6af97e8d6
quadritech <qmujeeb193@gmail.com>
2022-03-12 12:47:10 -0800

    Exemption of q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: print alphabets in lower case except q&e
+ *Return: 0
+ */
+int main(void)
+{
+
+char lettiers = 'a';
+
+while (letters <= 'z')
+{
+	if (letters == 'e' || letters == 'q')
+	{
+	}
+	else
+	{
+	putchar(letters++);
+}
+putchar('\n');
+return (0);
+}
