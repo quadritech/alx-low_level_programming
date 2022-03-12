9ab599d1319f0b55736bb601d11ece58215af156
quadritech <qmujeeb193@gmail.com>
2022-03-12 13:29:02 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Descrription: print all single digit numbers of base 10 from 0
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		printf(i);
+		i++
+	}
+	return (0)
+}
