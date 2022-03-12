ea0fbd72507b0172525e7a387c257b1d5f4a6117
quadritech <qmujeeb193@gmail.com>
2022-03-12 14:39:43 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: print alphabets in lower case and upper case
+ *Return: 0
+ */
+int main(void)
+{
+
+int num = 0;
+
+while ( <= 9)
+{
+	putchar(num);
+	num++;
+}
+num = 'a';
+while (small <= 'f')
+{
+	putchar(num);
+	num++;
+}
+putchar('\n');
+return (0);
+}
