d80d83ab413eade2b39dd1f2e398257218c1593e
quadritech <qmujeeb193@gmail.com>
2022-03-12 14:28:30 -0800

    lowercase letter in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: print alphabets in lower case
+ *Return: 0
+ */
+int main(void)
+{
+
+char letters = 'z';
+
+while (letters <= 'a')
+{
+	putchar(letters--);
+}
+putchar('\n');
+return (0);
+}
