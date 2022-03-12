635588046aaa19f273b2d329fa400df3638247b9
quadritech <qmujeeb193@gmail.com>
2022-03-12 09:27:24 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
-		print("Last digit of %d is %d and is greater than 5\n" n, m);
+		printf("Last digit of %d is %d and is greater than 5\n" n, m);
 	}
 	else if ((n % 10) == 0)
 	{
-		print("Last digit of %d is %d and is 0\n" n, m);
+		printf("Last digit of %d is %d and is 0\n" n, m);
 	}
 	else
 	{
-		print("Last digit of %d is %d and is less than 6 and not 0\n" n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n" n, m);
 	}
 	return (0);
 }
