3926763923502ab68697209fa03784a27636477c
quadritech <qmujeeb193@gmail.com>
2022-03-12 16:04:26 -0800

    possible combination of no`s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: print alphabets in lower case and upper case
+ *Return: 0
+ */
+int main(void)
+{
+
+int small = 0;
+
+while (small <= 9)
+{
+	putchar(small);
+	small++;
+}
+small = ', ';
+small = ' ';
+while (small <= 9)
+{
+	putchar(small);
+	small++;
+}
+putchar('\n');
+return (0);
+}
