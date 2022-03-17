d71ea04270b1ed108be5a04e66fa659e2c979253
quadritech <qmujeeb193@gmail.com>
2022-03-17 04:50:42 -0700

    Comment out task debuging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - causes an infinite loop
+ * Return: 0
+ */
 int main(void)
 {
         int i;
@@ -12,12 +12,12 @@ int main(void)
 	i = 0;
 
 	/*
-	*while (i < 10)
+	 *while (i < 10)
         {
-	*Infinite loop - No increase of variable
-	*putchar(i);
+	 *Infinite loop - No increase of variable
+	 *putchar(i);
         }
-	*/
+	 */
 
         printf("Infinite loop avoided! \\o/\n");
 	return (0);
