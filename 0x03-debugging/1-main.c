3cf2561a7b0280ac2adcf36792962041419abd31
quadritech <qmujeeb193@gmail.com>
2022-03-17 08:55:08 -0700

    updated commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 
         printf("Infinite loop incoming :(\n");
 
@@ -18,7 +18,6 @@ int main(void)
 	*putchar(i);
 	}
 	*/
-
         printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
