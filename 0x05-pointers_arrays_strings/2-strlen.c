a7be75b1ffe61f97676cb4bd019975a5a8b9c041
quadritech <qmujeeb193@gmail.com>
2022-03-21 15:08:01 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - to return length of string
+ * @s: char *s 
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	{
+		for (i = 0; str[i] !='\0'; i++);
+		return (0);
+	}
+}
